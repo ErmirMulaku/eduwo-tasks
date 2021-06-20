58c5717fb16c8708af8bbd08ef0f16ff24371622
Ermir Mulaku <ermir.mulaku@studentet.uni-pr.edu>
2021-06-20 17:40:15 +0200

    fix FlyOutMenu and add HandleLoadingState

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { useApiCall } from "../../lib/hooks/useApiCall";
 import { Country, getCountries } from "../../api/Country";
 import Container from "../../components/Container/Container";
@@ -9,8 +9,12 @@ import { ReactComponent as DescIcon } from "../../assets/icons/sort-desc.svg";
 
 import "./Home.scss";
 import cs from "classnames";
+import Button from "../../components/shared/Button/Button";
 
 const Home = () => {
+  const [isAscSortActive, setIsAscSortActive] = useState(false);
+  const [isDescSortActive, setIsDescSortActive] = useState(false);
+
   const params = useMemo(() => ({ pLimit: 10, pPage: 1 }), []);
   const { data, loading, error } = useApiCall(getCountries, [params]);
   const countries = data?.Response ?? ([] as Country[]);
@@ -22,8 +26,12 @@ const Home = () => {
           <SearchInput />
           <div className="Home__sort_icons">
             <AscIcon
-              className={cs("Home__sort", "Home__asc")}
-              onMouseEnter={() => console.log("saasas")}
+              className={cs(
+                "Home__sort",
+                "Home__asc",
+                isAscSortActive && "Home__asc--active"
+              )}
+              onClick={() => setIsAscSortActive(true)}
             />
             <DescIcon className="Home__sort" />
           </div>
