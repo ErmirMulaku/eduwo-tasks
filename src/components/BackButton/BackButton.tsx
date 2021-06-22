62d4821f8163d551553fd4d316e1083071faddc8
Ermir Mulaku <ermir.mulaku@studentet.uni-pr.edu>
2021-06-22 19:32:06 +0200

    fix back button style

--- src/components/BackButton/BackButton.tsx
+++ src/components/BackButton/BackButton.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Button } from "reactstrap";
 import { ReactComponent as BackIcon } from "../../assets/icons/BackIcon.svg";
-import "./BackButton.scss";
 import { useRouter } from "../../lib/hooks/useRouter";
+import "./BackButton.scss";
 
 const BackButton = () => {
   const router = useRouter();
   return (
     <Button className="BackButton" onClick={() => router.history.goBack()}>
-      <BackIcon className="BackButton__icon" width={24} height={24} />
+      <BackIcon className="BackButton__icon" width={18} height={18} />
       <span className="BackButton__text">Back Home</span>
     </Button>
   );
